0eadc0798ea218fa6e09aaf6c4a75d224ee36d44
Jared_loera <salazarloerajared@gmail.com>
2024-10-01 23:47:19 -0600

    en locales

--- src/environments/environment.api.ts
+++ src/environments/environment.api.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    baseUrl: 'https://servertorreon.ddns.net/api/',
-    socket: 'https://servertorreon.ddns.net/socket/'
+    baseUrl: 'http://localhost:3333/api/',
+    socket: 'http://localhost:3000'
   };
 
   export const environment = {
